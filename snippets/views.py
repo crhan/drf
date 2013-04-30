661362ef88cd0189805036b39b56352375b1bb87
ruohan.chen <crhan123@gmail.com>
2013-04-30 12:14:27 +0800

    adding required permission to views
    Signed-off-by: ruohan.chen <crhan123@gmail.com>

--- snippets/views.py
+++ snippets/views.py
@@ -1,7 +1,9 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework import generics
+from rest_framework import permissions
 from django.contrib.auth.models import User
 
 
@@ -11,6 +13,7 @@ class SnippetList(generics.ListCreateAPIView):
     """
     model = Snippet
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def pre_save(self, obj):
         obj.owner = self.request.user
@@ -22,6 +25,8 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     model = Snippet
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+            IsOwnerOrReadOnly,)
 
     def pre_save(self, obj):
         obj.owner = self.request.user
