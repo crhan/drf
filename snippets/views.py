0345e94ff21a4928220b0f0b2f30179641140515
ruohan.chen <crhan123@gmail.com>
2013-04-30 11:54:57 +0800

    adding owner to SnippetSerializer add UserList UserDetail
    Signed-off-by: ruohan.chen <crhan123@gmail.com>

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,8 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from snippets.serializers import UserSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -10,9 +12,32 @@ class SnippetList(generics.ListCreateAPIView):
     model = Snippet
     serializer_class = SnippetSerializer
 
+    def pre_save(self, obj):
+        obj.owner = self.request.user
+
+
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a snippet instance.
     """
     model = Snippet
     serializer_class = SnippetSerializer
+
+    def pre_save(self, obj):
+        obj.owner = self.request.user
+
+
+class UserList(generics.ListAPIView):
+    """
+    List all users
+    """
+    model = User
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    """
+    Retrieve a user instance
+    """
+    model = User
+    serializer_class = UserSerializer
