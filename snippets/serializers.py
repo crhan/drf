356c9e76784a15de5c1d80265e809547ffc7acf3
ruohan.chen <crhan123@gmail.com>
2013-04-30 14:34:49 +0800

    add api_root
    Signed-off-by: ruohan.chen <crhan123@gmail.com>

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,12 +3,15 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.Field(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight',
+            format='html')
+
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
-
-    owner = serializers.Field(source='owner.username')
+        fields = ('url', 'highlight', 'owner',
+                'title', 'code', 'linenos', 'language', 'style')
 
 
 class UserSerializer(serializers.ModelSerializer):
